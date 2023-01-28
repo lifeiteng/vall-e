1c1b29f001846129301f8884463dde14e45473cf
Feiteng <lifeiteng0422@163.com>
2023-01-29 00:38:41 +0800

    Fix icefall segmentation fault

--- egs/libritts/prepare.sh
+++ egs/libritts/prepare.sh
@@ -2,6 +2,9 @@
 
 set -eou pipefail
 
+# fix segmentation fault reported in https://github.com/k2-fsa/icefall/issues/674
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+
 nj=16
 stage=-1
 stop_stage=3
