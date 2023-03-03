6529273c596bfb9a715f5a637299373458015ab2
Feiteng <lifeiteng0422@163.com>
2023-03-03 21:52:13 +0800

    TransformerEncoderLayer not inherit from nn

--- valle/modules/transformer.py
+++ valle/modules/transformer.py
@@ -1,7 +1,8 @@
-from typing import Optional
+from typing import Callable, Optional, Union
 
 import torch
 from torch import Tensor, nn
+from torch.nn import functional as F
 
 
 class AdaptiveLayerNorm(nn.Module):
@@ -32,13 +33,64 @@ class AdaptiveLayerNorm(nn.Module):
         return weight * self.norm(input) + bias
 
 
-class TransformerEncoderLayer(nn.TransformerEncoderLayer):
-    def __init__(self, d_model: int, *args, **kwargs) -> None:
-        super(TransformerEncoderLayer, self).__init__(d_model, *args, **kwargs)
-        self.norm1 = AdaptiveLayerNorm(d_model, self.norm1)
-        self.norm2 = AdaptiveLayerNorm(d_model, self.norm2)
-        # make why_not_sparsity_fast_path != ""
-        self.norm1.eps += self.norm2.eps * 1.0001
+class TransformerEncoderLayer(nn.Module):
+    __constants__ = ["batch_first", "norm_first"]
+
+    def __init__(
+        self,
+        d_model: int,
+        nhead: int,
+        dim_feedforward: int = 2048,
+        dropout: float = 0.1,
+        activation: Union[str, Callable[[Tensor], Tensor]] = F.relu,
+        layer_norm_eps: float = 1e-5,
+        batch_first: bool = False,
+        norm_first: bool = False,
+        device=None,
+        dtype=None,
+    ) -> None:
+        factory_kwargs = {"device": device, "dtype": dtype}
+        super(TransformerEncoderLayer, self).__init__()
+        self.self_attn = nn.MultiheadAttention(
+            d_model,
+            nhead,
+            dropout=dropout,
+            batch_first=batch_first,
+            **factory_kwargs,
+        )
+
+        # Implementation of Feedforward model
+        self.linear1 = nn.Linear(d_model, dim_feedforward, **factory_kwargs)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(dim_feedforward, d_model, **factory_kwargs)
+
+        self.norm_first = norm_first
+        self.norm1 = nn.LayerNorm(d_model, eps=layer_norm_eps, **factory_kwargs)
+        self.norm2 = nn.LayerNorm(d_model, eps=layer_norm_eps, **factory_kwargs)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+        # Legacy string support for activation function.
+        if isinstance(activation, str):
+            activation = _get_activation_fn(activation)
+
+        # We can't test self.activation in forward() in TorchScript,
+        # so stash some information about it instead.
+        if activation is F.relu or isinstance(activation, torch.nn.ReLU):
+            self.activation_relu_or_gelu = 1
+        elif activation is F.gelu or isinstance(activation, torch.nn.GELU):
+            self.activation_relu_or_gelu = 2
+        else:
+            self.activation_relu_or_gelu = 0
+        self.activation = activation
+
+        self.ada_norm1 = AdaptiveLayerNorm(d_model, self.norm1)
+        self.ada_norm2 = AdaptiveLayerNorm(d_model, self.norm2)
+
+    def __setstate__(self, state):
+        super(TransformerEncoderLayer, self).__setstate__(state)
+        if not hasattr(self, "activation"):
+            self.activation = F.relu
 
     def forward(
         self,
@@ -70,17 +122,41 @@ class TransformerEncoderLayer(nn.TransformerEncoderLayer):
         x, stage_embedding = src
         if self.norm_first:
             x = x + self._sa_block(
-                self.norm1(x, stage_embedding), src_mask, src_key_padding_mask
+                self.ada_norm1(x, stage_embedding),
+                src_mask,
+                src_key_padding_mask,
             )
-            x = x + self._ff_block(self.norm2(x, stage_embedding))
+            x = x + self._ff_block(self.ada_norm2(x, stage_embedding))
         else:
-            x = self.norm1(
+            x = self.ada_norm1(
                 x + self._sa_block(x, src_mask, src_key_padding_mask),
                 stage_embedding,
             )
-            x = self.norm2(x + self._ff_block(x), stage_embedding)
+            x = self.ada_norm2(x + self._ff_block(x), stage_embedding)
 
         return (x, stage_embedding)
+
+    # self-attention block
+    def _sa_block(
+        self,
+        x: Tensor,
+        attn_mask: Optional[Tensor],
+        key_padding_mask: Optional[Tensor],
+    ) -> Tensor:
+        x = self.self_attn(
+            x,
+            x,
+            x,
+            attn_mask=attn_mask,
+            key_padding_mask=key_padding_mask,
+            need_weights=False,
+        )[0]
+        return self.dropout1(x)
+
+    # feed forward block
+    def _ff_block(self, x: Tensor) -> Tensor:
+        x = self.linear2(self.dropout(self.activation(self.linear1(x))))
+        return self.dropout2(x)
 
 
 class TransformerDecoderLayer(nn.TransformerDecoderLayer):
@@ -144,3 +220,14 @@ class TransformerDecoderLayer(nn.TransformerDecoderLayer):
             x = self.norm3(x + self._ff_block(x), stage_embedding)
 
         return (x, stage_embedding)
+
+
+def _get_activation_fn(activation: str) -> Callable[[Tensor], Tensor]:
+    if activation == "relu":
+        return F.relu
+    elif activation == "gelu":
+        return F.gelu
+
+    raise RuntimeError(
+        "activation should be relu/gelu, not {}".format(activation)
+    )
